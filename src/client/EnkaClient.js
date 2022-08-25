a6eab6121f4115268b5473469d3080f048a70378
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2022-08-25 19:20:23 +0900

    remove fetch for next commit

--- src/client/EnkaClient.js
+++ src/client/EnkaClient.js
@@ -2,8 +2,6 @@ const User = require("../models/User");
 const UserNotFoundError = require("../errors/UserNotFoundError");
 const { bindOptions } = require("../utils/utils");
 
-const fetch = require("node-fetch"); // for nodejs 16 or below
-
 const getUserUrl = (uid) => `https://enka.network/u/${uid}/__data.json`;
 
 module.exports = class EnkaClient {
