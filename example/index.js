f599be1e326217c4bae7d15870d98c049d328459
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2022-10-05 19:11:50 +0900

    add skill description

--- example/index.js
+++ example/index.js
@@ -6,6 +6,8 @@ run();
 async function run() {
     const client = new EnkaClient({ timeout: 4000 });
     // await client.cachedAssetsManager.fetchAllContents();
-    console.log(await client.fetchUser(825436941));
+    const user = await client.fetchUser(825436941);
+
+    console.log(user.avatarInfoList[0].avatar.skills[0].description.get("jp"));
     setInterval(() => { }, 1000);
 }
--- src/models/character/Skill.js
+++ src/models/character/Skill.js
@@ -24,6 +24,9 @@ module.exports = class Skill {
         /** @type {TextAssets} */
         this.name = new TextAssets(this._data.nameTextMapHash, enka);
 
+        /** @type {TextAssets} */
+        this.description = new TextAssets(this._data.descTextMapHash, enka);
+
         /** @type {ImageAssets} */
         this.icon = new ImageAssets(this._data.skillIcon);
     }
