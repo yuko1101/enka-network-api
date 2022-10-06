05b0ed34568a766e49e713c1d18fe01c6746f140
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2022-10-06 18:00:34 +0900

    add property `isAvailable` and types

--- src/models/assets/ImageAssets.js
+++ src/models/assets/ImageAssets.js
@@ -5,7 +5,13 @@ module.exports = class ImageAssets {
      * @param {string} name
      */
     constructor(name) {
+        /** @type {string} */
         this.name = name;
+
+        /** @type {string} */
         this.url = name === "" ? "" : `${imageBaseUrl}/${name}.png`;
+
+        /** @type {boolean} */
+        this.isAvailable = this.name !== null && this.name !== undefined && this.name !== "";
     }
 }
