473f219bb700324988f790d3b8ca00a1b5aa5737
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2022-11-13 13:22:31 +0900

    avoid downloading cache when install other packages

--- scripts/install.js
+++ scripts/install.js
@@ -1,7 +1,15 @@
-const nocache = process.env.npm_config_enka_nocache;
-if (nocache === "true" || nocache === "1") return;
-
 const fs = require("fs");
+
+const nocache = process.env.npm_config_enka_nocache;
+
+if (nocache === "true" || nocache === "1") {
+    if (!fs.existsSync("cache")) {
+        // create folder not to download the data when install other packages.
+        fs.mkdirSync("cache");
+    }
+    return;
+}
+
 if (fs.existsSync("cache")) return;
 
 const { Axios } = require("axios");
