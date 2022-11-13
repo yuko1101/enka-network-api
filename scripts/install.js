5d87a630893eb84ccf895385097d1b3137209980
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2022-11-13 12:17:57 +0900

    rename

--- scripts/install.js
+++ scripts/install.js
@@ -1,13 +1,18 @@
 const nocache = process.env.npm_config_enka_nocache;
 if (nocache === "true" || nocache === "1") return;
 
-const request = require("request");
 const fs = require("fs");
+if (fs.existsSync("cache")) return;
+
+const request = require("request");
 const unzipper = require("unzipper");
 
 request.get("https://raw.githubusercontent.com/yuko1101/enka-network-api/main/cache.zip")
     .pipe(fs.createWriteStream("cache.zip"))
     .on("close", () => {
         fs.createReadStream("cache.zip")
-            .pipe(unzipper.Extract({ path: "./" }));
+            .pipe(unzipper.Extract({ path: "./" }))
+            .on("close", () => {
+                fs.rmSync("cache.zip");
+            });
     });
