2c6816f371164da1a1b628e53ffafe5aca073171
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2022-11-13 11:51:25 +0900

    use unzipper instead of unzip

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,13 +1,13 @@
-const nocache = npm_config_enka_nocache;
+const nocache = process.env.npm_config_enka_nocache;
 if (nocache === "true" || nocache === "1") return;
 
 const request = require("request");
 const fs = require("fs");
-const unzip = require("unzip");
+const unzipper = require("unzipper");
 
 request.get("https://raw.githubusercontent.com/yuko1101/enka-network-api/main/cache.zip")
     .pipe(fs.createWriteStream("cache.zip"))
     .on("close", () => {
         fs.createReadStream("cache.zip")
-            .pipe(unzip.Extract({ path: "./" }));
+            .pipe(unzipper.Extract({ path: "./" }));
     });
