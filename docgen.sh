03501e2399234fa00c18e17ee7187418b5a2123a
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2022-11-19 14:06:22 +0900

    new docs

--- docgen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-workDir=$(cd "$0/.." && pwd)
-templateDir="$USERPROFILE/AppData/Roaming/npm/node_modules/docdash" # from global
-
-cd "${workDir}"
-
-jsdoc "${workDir}/src" -r -t "${templateDir}" -d "docs" --debug
-
-read -p "Press [Enter] key to resume."
