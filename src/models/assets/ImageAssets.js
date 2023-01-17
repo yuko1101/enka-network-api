56b085d8253e6db8006b2f4be53ccaf8ea2f672f
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2023-01-17 19:06:05 +0900

    mihoyo resource url (WIP)

--- src/models/assets/ImageAssets.js
+++ src/models/assets/ImageAssets.js
@@ -1,4 +1,13 @@
-const imageBaseUrl = "https://enka.network/ui"
+const imageBaseUrl = "https://enka.network/ui";
+const imageBaseUrlMihoyo = "https://upload-os-bbs.mihoyo.com/game_record/genshin";
+
+/** @type {object} */
+const imageTypes = {
+    "character_side_icon": [/^UI_AvatarIcon_Side_(.+)$/],
+    "character_icon": [/^UI_AvatarIcon_(.+)$/],
+    "equip": [/^UI_EquipIcon_(.+?)(_Awaken)?$/, /^UI_RelicIcon_(.+)$/],
+    "constellation_icon": [/^UI_Talent_(.+)$/],
+}
 
 /** 
  * @en ImageAssets
@@ -14,6 +23,14 @@ class ImageAssets {
 
         /** @type {string} */
         this.url = name === "" ? "" : `${imageBaseUrl}/${name}.png`;
+
+        /** @type {string} */
+        this.imageType = Object.keys(imageTypes).find(type => imageTypes[type].some(regex => regex.test(name)));
+
+        // if (!this.imageType) console.log(name);
+
+        /** @type {string} */
+        this.mihoyoUrl = (name === "" || !this.imageType) ? "" : `${imageBaseUrlMihoyo}/${name}.png`;
 
         /** @type {boolean} */
         this.isAvailable = this.name !== null && this.name !== undefined && this.name !== "";
