4a5ca46a67c3c901920e0cc9b7baadd1a8f88a74
yuko1101 <yukoserver@gmail.com>
2023-05-24 12:29:46 +0900

    improve code quality

--- scripts/install.js
+++ scripts/install.js
@@ -3,7 +3,11 @@ const fs = require("fs");
 const nocache = process.env.npm_config_enka_nocache;
 const ghproxy = process.env.npm_config_enka_ghproxy;
 
-if (nocache === "true" || nocache === "1") {
+function getBool(value) {
+    return value?.toLowerCase() === "true" || value === "1";
+}
+
+if (getBool(nocache)) {
     if (!fs.existsSync("cache")) {
         // create folder not to download the data when install other packages.
         fs.mkdirSync("cache");
@@ -16,4 +20,4 @@ if (fs.existsSync("cache")) return;
 const { EnkaClient } = require("..");
 const enka = new EnkaClient();
 
-enka.cachedAssetsManager._downloadCacheZip({ ghproxy: ghproxy === "true" || ghproxy === "1" });
+enka.cachedAssetsManager._downloadCacheZip({ ghproxy: getBool(ghproxy) });
