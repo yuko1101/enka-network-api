72fd0a142415119cc256363eab8c94169141c9c8
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2023-07-27 18:17:17 +0900

    Avoid js/prototype-polluting-assignment

--- src/client/CachedAssetsManager.ts
+++ src/client/CachedAssetsManager.ts
@@ -404,7 +404,7 @@ class CachedAssetsManager {
      */
     getLanguageData(lang: LanguageCode, directory?: string): { [key: string]: string } {
         // Avoid "js/prototype-polluting-assignment" just in case. (https://github.com/yuko1101/enka-network-api/security/code-scanning/252)
-        if (lang as unknown === "__proto__") return {};
+        if (["__proto__", "constructor", "prototype"].includes(lang)) return {};
         langDataMemory[lang] ??= JSON.parse(fs.readFileSync(this.getLanguageDataPath(lang), "utf-8"));
         if (directory) {
             const loadedJson = JSON.parse(fs.readFileSync(this.getLanguageDataPath(lang, directory), "utf-8"));
