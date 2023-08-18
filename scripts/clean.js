6fdd3f54d7f8b9a285dbdc93ffd4e229c70fc8d2
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2023-08-18 19:23:15 +0900

    fixed npm build scripts

---
+++ scripts/clean.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+if (fs.existsSync("dist")) {
+    deleteFiles("dist");
+}
+
+
+
+function deleteFiles(path, remove) {
+    const target = fs.statSync(path);
+    if (!target.isDirectory()) {
+        fs.unlinkSync(path);
+        return;
+    }
+    const files = fs.readdirSync(path);
+    for (const file of files) {
+        deleteFiles(path + "/" + file);
+    }
+}
